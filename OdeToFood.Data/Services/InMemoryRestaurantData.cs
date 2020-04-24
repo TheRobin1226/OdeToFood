7cb303a13ea892f8fdbc3a2a77c7b43223103550
TheRobin1226 <mrobinson.ua@gmail.com>
2020-04-24 16:55:11 -0400

    Initial commit

---
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OdeToFood.Data.Models;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Dorado Tacos", Cuisine = CuisineType.Mexican },
+                new Restaurant { Id = 2, Name = "Panda Express", Cuisine = CuisineType.Chinese },
+                new Restaurant { Id = 3, Name = "Pizza Pizza", Cuisine = CuisineType.Italian },
+            };
+        }
+
+        public Restaurant Get(int id)
+        {
+            return restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
---
+++ OdeToFood.Web/App_Start/ContainerConfig.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using OdeToFood.Data.Services;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace OdeToFood.Web
+{
+    public class ContainerConfig
+    {
+        internal static void RegisterContainer(HttpConfiguration httpConfiguration)
+        {
+            var builder = new ContainerBuilder();
+
+            // scan through your project for the controller types you pass in
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterApiControllers(typeof(MvcApplication).Assembly);
+
+            builder.RegisterType<InMemoryRestaurantData>()
+                .As<IRestaurantData>()
+                .SingleInstance();
+
+            var container = builder.Build();
+
+            // telling the MVC 5 framework whenever you are resolving dependencies, use this containter
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            httpConfiguration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+    }
+}
